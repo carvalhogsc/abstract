96693152132dc7958036caeed30d3b1306c61fe6
Guilherme Carvalho <carvalhogsc@gmail.com>
2018-10-30 22:33:42 -0300

    Classes Finalizadas

--- src/entities/CompanyTax.java
+++ src/entities/CompanyTax.java
@@ -1,5 +1,38 @@
 package entities;
 
-public class CompanyTax {
+public class CompanyTax extends TaxPayer {
+	
+	private Integer numberEmployees;
+	
+	public CompanyTax() {
+		super();
+	}
+
+	public CompanyTax(String name, Double anualIncome, Integer numberEmployees) {
+		super(name, anualIncome);
+		this.numberEmployees = numberEmployees;
+	}
+
+	public Integer getNumberEmployees() {
+		return numberEmployees;
+	}
+
+	public void setNumberEmployees(Integer numberEmployees) {
+		this.numberEmployees = numberEmployees;
+	}
+
+	@Override
+	public double tax() {
+		double tax;
+		
+		if(numberEmployees > 10) {
+			tax = getAnualIncome() * 0.14;
+		} else {
+			tax = getAnualIncome() * 0.16;
+		}		
+		return tax;
+	}
+	
+	
 
 }
