485f12f477ec92094e6ff4670cb9271cc7cfefe1
Guilherme Carvalho <carvalhogsc@gmail.com>
2018-10-30 22:28:30 -0300

    Classe IndividualTax construtores e setters e getters e Criação Classe CompanyTax

--- src/entities/IndividualTax.java
+++ src/entities/IndividualTax.java
@@ -3,6 +3,23 @@ package entities;
 public class IndividualTax extends TaxPayer {
 
 	private Double healthExpenditures;
+	
+	public IndividualTax() {
+
+	}
+	
+	public IndividualTax(String name, Double anualIncome, Double healthExpenditures) {
+		super(name, anualIncome);
+		this.healthExpenditures = healthExpenditures;
+	}
+	
+	public Double getHealthExpenditures() {
+		return healthExpenditures;
+	}
+
+	public void setHealthExpenditures(Double healthExpenditures) {
+		this.healthExpenditures = healthExpenditures;
+	}
 
 	@Override
 	public double tax() {
